a33d46a16307a9442b1854647164d62132bb07b4
Bennie Rosas <benjamin.rosas@dockyard.com>
2021-12-08 23:05:52 -0600

    Add oh-my-zsh plugins. Move aliases and exports to oh-my-zsh custom directory. Prefer newer ed25519 ssh key protocol. Generally update and clean things up. ShellCheck fixes to install.sh Markdown lint fixes to README.md, and update README. Remove opinionated tmux configuration. Remove opinionated vim configuration. Allow oh-my-zsh script to set up default .zshrc. Prefer spaces to tabs :crossed_swords: in editorconfig.

---
+++ zshfiles/exports.zsh
@@ -0,0 +1,5 @@
+# use Vim as editor
+export VISUAL=vim
+export EDITOR=$VISUAL
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
---
+++ zshfiles/plugins.zsh
@@ -0,0 +1,9 @@
+# Add zsh plugins https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins
+plugins+=(
+  ag
+  asdf
+  git
+  gitignore
+  macos
+  tmux
+)
