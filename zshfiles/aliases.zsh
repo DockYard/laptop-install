889ae0aa30088bfe8c9994b8e0effa939ff95e77
Bennie Rosas <ben@aliencyb.org>
2021-12-13 14:58:02 -0600

    fix issues with colorflag

--- zshfiles/aliases.zsh
+++ zshfiles/aliases.zsh
@@ -15,12 +15,7 @@ alias j="jobs"
 alias e='$EDITOR'
 alias v='$VISUAL'
 
-# Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-  colorflag="--color"
-else # OS X `ls`
-  colorflag="-G"
-fi
+colorflag="-G"
 
 # List all files colorized in long format
 alias l='ls -lhF ${colorflag}'
